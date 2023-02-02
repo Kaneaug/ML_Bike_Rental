db64b2702d4c7ee893943e3b9c0e87c665de0eb4
Kaneaug <kane.monaco@gmail.com>
2023-02-01 16:04:11 -0800

    Added MSE and MAE tests

--- ML_bike_rental.py
+++ ML_bike_rental.py
@@ -1,25 +1,30 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-# Load the dataset into a pandas dataframe
-file_path = r'C:\Users\kanem\Documents\ML_Bike_Rental\data\daily-bike-share.csv'
+# Load the data into a pandas dataframe
+file_path = r"C:\Users\kanem\Documents\ML_Bike_Rental\data\raw\daily-bike-share.csv"
 df = pd.read_csv(file_path)
 
-# Split the data into features and target
-X = df.drop(['dteday', 'rentals'], axis=1)
-y = df['rentals']
+# Define the features and target
+features = ['temp', 'hum', 'windspeed', 'weekday', 'workingday', 'weathersit']
+target = 'rentals'
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.2, random_state=42)
 
-# Train a linear regression model
+# Train the linear regression model
 reg = LinearRegression().fit(X_train, y_train)
 
-# Make predictions on the test set
+# Use the model to make predictions on the test data
 y_pred = reg.predict(X_test)
 
-# Evaluate the model performance using mean squared error
+# Calculate the mean squared error (MSE) and mean absolute error (MAE) as test scores
 mse = mean_squared_error(y_test, y_pred)
-print(f'Mean Squared Error: {mse}')
+mae = mean_absolute_error(y_test, y_pred)
+
+# Print the test scores
+print("Mean Squared Error:", mse)
+print("Mean Absolute Error:", mae)
